43c8810f0d215db7a3787af99b350fd05b33f1eb
SDA Renovation <cris.siadoua@gmail.com>
2026-06-19 17:03:38 +0200

    fix: dynamic import Editeur3D with ssr:false to avoid Three.js WebGL crash on server

--- src/app/(app)/conception/3d/[id]/page.tsx
+++ src/app/(app)/conception/3d/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { notFound } from "next/navigation";
 import { prisma } from "@/lib/prisma";
-import { Editeur3D } from "./editeur-3d";
+import dynamic from "next/dynamic";
+
+const Editeur3D = dynamic(() => import("./editeur-3d").then((m) => m.Editeur3D), { ssr: false });
 
 export default async function Projet3DPage({
   params,
